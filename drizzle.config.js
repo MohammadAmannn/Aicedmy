3cb131375db10f3f4cbf37804256ab62a46dba1e
Aman <135089094+MohammadAmannn@users.noreply.github.com>
2025-02-09 12:39:16 +0530

    Remove drizzle.config.js from repository

--- drizzle.config.js
+++
@@ -1,8 +0,0 @@
-/** @type { import("drizzle-kit").Config } */
-export default {
-    schema: "./configs/schema.jsx",
-    dialect: 'postgresql',
-    dbCredentials: {
-        url: 'postgresql://neondb_owner:D1TkPy6igrVO@ep-mute-term-a5fb4kjo.us-east-2.aws.neon.tech/neondb?sslmode=require',
-    }
-}
